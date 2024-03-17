fe52ee3cb565dcf16e634eae9e2384755132602b
Xharless <carlosarevalo1702@hotmail.com>
2024-03-17 18:53:44 -0300

    subir archivos lab 1

---
+++ base_t1_arqui.py
@@ -0,0 +1,44 @@
+
+import random
+from prettytable import PrettyTable
+
+def crear_mapa(size,cantidad_naves):
+    listaposnaves = []
+    lista = []
+    for i in range(0,size*2):
+        row = []
+        for j in range(0,size):
+            row.append('?')
+        lista.append(row)
+
+    for i in range(0,cantidad_naves):
+        cordx = random.randint(0,size-1)
+        cordy = random.randint(0,size-1)
+        tipo = random.randint(0,2)
+        tipos = 'XYZ'
+        lista[cordx][cordy] = tipos[tipo]
+        listaposnaves.append((cordx,cordy))
+
+
+    return(lista,listaposnaves)
+
+def visualizar_mapa(lista):
+    
+    tabla = PrettyTable()
+
+    
+    tabla.field_names = [''] + list(range(len(lista[0])))
+
+    
+    for numberfila, row in enumerate(lista):
+        row_data = [numberfila] + row
+        tabla.add_row(row_data)
+
+    
+    print(tabla)
+
+
+juego,enemigos = crear_mapa(5,5)
+visualizar_mapa(juego)
+
+a = input()
